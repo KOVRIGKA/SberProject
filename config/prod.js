e9d81c72925106599bea2eeb52567e1ae49b374d
Анастасия Ковригина <anastasiakovrigina@MacBook-Pro-Anastasia.local>
2019-11-24 00:00:31 +0300

    finished auth

---
+++ config/prod.js
@@ -0,0 +1,17 @@
+// production keys here 
+
+//do not commit it
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
+
+// eEkeA7cxC8QZtvul
+// mongoURI: 'mongodb+srv://user1:eEkeA7cxC8QZtvul@emaily-4t8od.mongodb.net/test?retryWrites=true&w=majority',
+
+//0.0.0.0/0 S9BZ87bScRbbj7kq
+
+// mongodb+srv://myuser:<password>@cluster0-6o7nv.mongodb.net/test?retryWrites=true&w=majority
